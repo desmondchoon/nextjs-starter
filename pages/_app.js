5efb84abcd0154a050dbff1e1bffe20ec332b35a
Desmond <desmond@desmondchin.xyz>
2019-08-28 11:20:17 +0800

    update for new Material UI version

--- pages/_app.js
+++ pages/_app.js
@@ -1,79 +1,33 @@
 import React from 'react';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import Head from 'next/head';
-import { MuiThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import JssProvider from 'react-jss/lib/JssProvider';
-import getPageContext from '../src/getPageContext';
-import { initializeStore } from '../store';
-import { Provider } from 'mobx-react';
+import theme from '../src/theme';
 
-class MyApp extends App {
-
-  static async getInitialProps(appContext) {
-    // Get or Create the store with `undefined` as initialState
-    // This allows you to set a custom default initialState
-    const mobxStore = initializeStore()
-    // Provide the store to getInitialProps of pages
-    appContext.ctx.mobxStore = mobxStore
-
-    let appProps = await App.getInitialProps(appContext)
-
-    return {
-      ...appProps,
-      initialMobxState: mobxStore
-    }
-  }
-
-  constructor(props) {
-    super(props);
-    this.pageContext = getPageContext();
-    const isServer = !process.browser
-    this.mobxStore = isServer
-      ? props.initialMobxState
-      : initializeStore({
-        /**Your initial props here */
-      })
-  }
-
+export default class MyApp extends App {
   componentDidMount() {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles && jssStyles.parentNode) {
+    if (jssStyles) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
   }
 
   render() {
     const { Component, pageProps } = this.props;
+
     return (
-      <Container>
+      <React.Fragment>
         <Head>
           <title>My page</title>
         </Head>
-        {/* Wrap every page in Jss and Theme providers */}
-        <JssProvider
-          registry={this.pageContext.sheetsRegistry}
-          generateClassName={this.pageContext.generateClassName}
-        >
-          {/* MuiThemeProvider makes the theme available down the React
-              tree thanks to React context. */}
-          <MuiThemeProvider
-            theme={this.pageContext.theme}
-            sheetsManager={this.pageContext.sheetsManager}
-          >
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            {/* Pass pageContext to the _document though the renderPage enhancer
-                to render collected styles on server-side. */}
-                <Provider store={this.mobxStore}>
-                  <Component pageContext={this.pageContext} {...pageProps} />
-                </Provider>
-          </MuiThemeProvider>
-        </JssProvider>
-      </Container>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </React.Fragment>
     );
   }
 }
-
-export default MyApp;
