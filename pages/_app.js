4e4d21f2bc89d8b11f7225365b3eb9551c92243f
Desmond <desmond@desmondchin.xyz>
2019-08-28 11:34:31 +0800

    added mobx to new material UI upgrade

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,36 @@ import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
+import { Provider } from 'mobx-react';
+import { initializeStore } from '../store';
 
 export default class MyApp extends App {
+
+  static async getInitialProps(appContext) {
+    // Get or Create the store with `undefined` as initialState
+    // This allows you to set a custom default initialState
+    const mobxStore = initializeStore()
+    // Provide the store to getInitialProps of pages
+    appContext.ctx.mobxStore = mobxStore
+
+    let appProps = await App.getInitialProps(appContext)
+
+    return {
+      ...appProps,
+      initialMobxState: mobxStore
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    const isServer = !process.browser
+    this.mobxStore = isServer
+      ? props.initialMobxState
+      : initializeStore({
+        /**Your initial props here */
+      })
+  }
+
   componentDidMount() {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
@@ -25,7 +53,9 @@ export default class MyApp extends App {
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
-          <Component {...pageProps} />
+          <Provider store={this.mobxStore}>
+            <Component pageContext={this.pageContext} {...pageProps} />
+          </Provider>
         </ThemeProvider>
       </React.Fragment>
     );
